8e57acb43aeef879471bd5a447d1df446bd0f448
Thiago Lima <thiagolima86@gmail.com>
2021-03-17 19:32:11 -0300

    correções yohan

--- yohan_dev_install.sh
+++ yohan_dev_install.sh
@@ -14,16 +14,15 @@ docker-compose build
 docker-compose run --rm web bundle install
 
 
-read -p "\n\nDeseja criar database.yml? Y/n: " has_db_file
+read -p "Deseja criar database.yml? Y/n: " has_db_file
 if [ $has_db_file != "n" ]
 then
- read -p "\n\nNome do banco: " db_name
+ read -p "Nome do banco: " db_name
  db_file="config/database.yml"
- db_yml="
-default: &default
+ db_yml="default: &default
   adapter: postgresql
   encoding: unicode
-  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+  pool: 5
   host: postgres
   user: postgres
   password: mypassword
@@ -32,10 +31,10 @@ development:
   database: $db_name
 test:
   <<: *default
-  database: '$db_name'_test
+  database: ${db_name}_test
 "
 
-  echo $db_yml >> $db_file
+  echo "$db_yml" > $db_file
 fi 
 
 #cria o banco
@@ -43,5 +42,5 @@ docker-compose run --rm web bundle exec rails db:create
 #executa migrations
 docker-compose run --rm web bundle exec rails db:migrate
 
-echo "\n\n Instalado com sucesso";
+echo "Instalado com sucesso";
 
